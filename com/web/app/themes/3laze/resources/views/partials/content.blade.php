3fb2605ab1151c0d9b9b9104e66b1497a59b05f6
Jon Ouellette <jonaouellette@gmail.com>
2018-10-09 12:09:15 -0700

    Removed header title and reformatted cards.

--- com/web/app/themes/3laze/resources/views/partials/content.blade.php
+++ com/web/app/themes/3laze/resources/views/partials/content.blade.php
@@ -1,12 +1,19 @@
 <article @php post_class() @endphp>
-  <header>
-    @if ( has_post_thumbnail() )
-      {{ the_post_thumbnail() }}
-    @endif
-    <h2 class="entry-title"><a href="{{ get_permalink() }}">{{ get_the_title() }}</a></h2>
-  </header>
-  <div class="entry-summary">
-    @php the_excerpt() @endphp
-    @include('partials/entry-meta')
+  <div class="max-w-sm rounded overflow-hidden shadow-lg mb-5">
+    <img class="w-full" src="{{ get_the_post_thumbnail_url() }}" alt="">
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">
+        <a href="{{ get_permalink() }}">{{ get_the_title() }}</a>
+      </div>
+      <p class="text-grey-darker text-base">
+        @php the_excerpt() @endphp
+      </p>
+    </div>
+    <div class="px-6 py-4 flex justify-around">
+      {!! get_the_tag_list('<span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">', '</span><span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2"> ', '</span>') !!}
+    </div>
+    <div class="flex justify-end p-2">
+      @include('partials/entry-meta')
+    </div>
   </div>
 </article>
